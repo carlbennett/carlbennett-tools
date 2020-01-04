924647587658e7c48e48069b321f641ed699442b
Carl Bennett <carl@carlbennett.me>
2020-01-03 22:07:03 -0600

    Remove _responseTTL from all controllers

--- src/controllers/Gandalf.php
+++ src/controllers/Gandalf.php
@@ -17,7 +17,6 @@ class Gandalf extends Controller {
 
     $model->_responseCode                    = 200;
     $model->_responseHeaders["Content-Type"] = $view->getMimeType();
-    $model->_responseTTL                     = 0;
 
     return $model;
 
