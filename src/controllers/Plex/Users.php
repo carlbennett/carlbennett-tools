b7ec0b24766009527ac27cbf26e9c9102db460d8
Carl Bennett <carl@carlbennett.me>
2020-01-07 02:01:58 -0600

    Add login gate for Plex Users page

--- src/controllers/Plex/Users.php
+++ src/controllers/Plex/Users.php
@@ -6,17 +6,25 @@ use \CarlBennett\MVC\Libraries\Controller;
 use \CarlBennett\MVC\Libraries\Router;
 use \CarlBennett\MVC\Libraries\View;
 
-use \CarlBennett\Tools\Libraries\Plex\User;
+use \CarlBennett\Tools\Libraries\Authentication;
+use \CarlBennett\Tools\Libraries\Plex\User as PlexUser;
+use \CarlBennett\Tools\Libraries\User;
 use \CarlBennett\Tools\Models\Plex\Users as UsersModel;
 
 class Users extends Controller {
   public function &run(Router &$router, View &$view, array &$args) {
     $model = new UsersModel();
 
-    $model->users = User::getAll();
+    $user = Authentication::$user;
+
+    if (!$user || !($user->getOptionsBitmask() & User::OPTION_ACL_PLEX_USERS)) {
+      $model->users = false;
+    } else {
+      $model->users = PlexUser::getAll();
+    }
 
     $view->render($model);
-    $model->_responseCode = 200;
+    $model->_responseCode = ($model->users ? 200 : 401);
     return $model;
   }
 }
