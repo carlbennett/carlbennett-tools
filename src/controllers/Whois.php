1db76041cc683920d83804245273c25109e36926
Carl Bennett <carl@carlbennett.me>
2020-01-07 22:18:22 -0600

    Fix user input escape

--- src/controllers/Whois.php
+++ src/controllers/Whois.php
@@ -22,7 +22,7 @@ class Whois extends Controller {
     } else {
       $r = (!$model->recursive ? '-n ' : '');
       $model->query_result = shell_exec(
-        'whois "' . $r . escapeshellarg($model->query) . '" 2>&1'
+        'whois ' . $r . '"' . escapeshellarg($model->query) . '" 2>&1'
       );
     }
 
