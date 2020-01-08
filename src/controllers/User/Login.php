dadfc3d3c9fc39a25b421ba81d5e13c49f77d8fc
Carl Bennett <carl@carlbennett.me>
2020-01-07 23:47:03 -0600

    Add a little logic to the user login page

--- src/controllers/User/Login.php
+++ src/controllers/User/Login.php
@@ -10,6 +10,15 @@ use \CarlBennett\Tools\Models\User\Login as LoginModel;
 class Login extends Controller {
   public function &run(Router &$router, View &$view, array &$args) {
     $model = new LoginModel();
+
+    $form = $router->getRequestBodyArray();
+    $model->email = (isset($form['email']) ? $form['email'] : null);
+    $model->password = (isset($form['password']) ? $form['password'] : null);
+
+    if ($router->getRequestMethod() == 'POST') {
+      $model->error = 'Incorrect password.';
+    }
+
     $view->render($model);
     $model->_responseCode = 200;
     return $model;
--- src/models/User/Login.php
+++ src/models/User/Login.php
@@ -4,4 +4,10 @@ namespace CarlBennett\Tools\Models\User;
 
 use \CarlBennett\MVC\Libraries\Model;
 
-class Login extends Model {}
+class Login extends Model {
+
+  public $email;
+  public $error;
+  public $password;
+
+}
