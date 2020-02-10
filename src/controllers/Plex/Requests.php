6226895092abdeb088c8beb42939d791e36090a2
Carl Bennett <carl@carlbennett.me>
2020-02-10 01:39:56 -0600

    Add login gate for plex requests page

--- src/controllers/Plex/Requests.php
+++ src/controllers/Plex/Requests.php
@@ -6,11 +6,23 @@ use \CarlBennett\MVC\Libraries\Controller;
 use \CarlBennett\MVC\Libraries\Router;
 use \CarlBennett\MVC\Libraries\View;
 
-use \CarlBennett\Tools\Models\Plex\Requests as RequestsModel;
+use \CarlBennett\Tools\Libraries\Authentication;
+use \CarlBennett\Tools\Libraries\User;
+use \CarlBennett\Tools\Models\Plex\Requests as PlexRequestsModel;
 
 class Requests extends Controller {
   public function &run(Router &$router, View &$view, array &$args) {
-    $model = new RequestsModel();
+    $model = new PlexRequestsModel();
+    $model->active_user = Authentication::$user;
+
+    if (!($model->active_user && (
+      $model->active_user->getOptionsBitmask() & User::OPTION_ACL_PLEX_REQUESTS
+    ))) {
+      $view->render($model);
+      $model->_responseCode = 401;
+      return $model;
+    }
+
     $view->render($model);
     $model->_responseCode = 200;
     return $model;
--- src/models/Plex/Requests.php
+++ src/models/Plex/Requests.php
@@ -4,4 +4,8 @@ namespace CarlBennett\Tools\Models\Plex;
 
 use \CarlBennett\MVC\Libraries\Model;
 
-class Requests extends Model {}
+class Requests extends Model {
+
+  public $active_user;
+
+}
