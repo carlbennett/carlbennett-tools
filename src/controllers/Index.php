94781cad6ca71e9fe60269a924001a483753b5ae
Carl Bennett <carl@carlbennett.me>
2021-04-18 02:44:23 -0500

    Freshen up the toolbox homepage

--- src/controllers/Index.php
+++ src/controllers/Index.php
@@ -5,11 +5,13 @@ namespace CarlBennett\Tools\Controllers;
 use \CarlBennett\MVC\Libraries\Controller;
 use \CarlBennett\MVC\Libraries\Router;
 use \CarlBennett\MVC\Libraries\View;
+use \CarlBennett\Tools\Libraries\Authentication;
 use \CarlBennett\Tools\Models\Index as IndexModel;
 
 class Index extends Controller {
   public function &run(Router &$router, View &$view, array &$args) {
     $model = new IndexModel();
+    $model->active_user = Authentication::$user;
     $model->routes = $router->getRoutes();
     $view->render($model);
     $model->_responseCode = 200;
--- src/models/Index.php
+++ src/models/Index.php
@@ -6,6 +6,7 @@ use \CarlBennett\MVC\Libraries\Model;
 
 class Index extends Model {
 
+  public $active_user;
   public $routes;
 
 }
