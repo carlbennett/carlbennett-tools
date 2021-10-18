5958210d1efea710164e1a8bc72fa301bef135db
Carl Bennett <carl@carlbennett.me>
2021-10-18 03:51:06 -0500

    Add GeoIP from MaxMind

---
+++ src/libraries/GeoIP.php
@@ -0,0 +1,54 @@
+<?php
+namespace CarlBennett\Tools\Libraries;
+
+use \CarlBennett\MVC\Libraries\Common;
+use \GeoIp2\Database\Reader;
+use \GeoIp2\Exception\AddressNotFoundException;
+use \MaxMind\Db\InvalidDatabaseException;
+use \UnexpectedValueException;
+
+class GeoIP
+{
+  private static $reader;
+
+  private function __construct() {}
+
+  protected static function getReader()
+  {
+    if (self::$reader) return self::$reader;
+
+    try
+    {
+      self::$reader = new Reader(Common::$config->geoip->database_file);
+    }
+    catch (InvalidDatabaseException $e)
+    {
+      // database is invalid or corrupt
+      self::$reader = null;
+    }
+
+    return self::$reader;
+  }
+
+  public static function getRecord(string $address)
+  {
+    if (!filter_var($address, FILTER_VALIDATE_IP))
+    {
+      throw new UnexpectedValueException('not a valid IP address');
+    }
+
+    $mmdb = self::getReader();
+    $type = Common::$config->geoip->database_type;
+
+    try
+    {
+      $record = $mmdb->$type($address);
+    }
+    catch (AddressNotFoundException $e)
+    {
+      $record = null;
+    }
+
+    return $record;
+  }
+}
