19df8523cae40b11e3e84df8212ef1f3257729ec
Carl Bennett <carl@carlbennett.me>
2022-03-27 06:18:52 -0500

    Implement basic smtp wrapper

---
+++ src/libraries/Mailer.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace CarlBennett\Tools;
+
+use \CarlBennett\MVC\Libraries\Common;
+use \CarlBennett\Tools\Libraries\Authentication;
+
+use \PHPMailer\PHPMailer\Exception as PHPMailerException;
+use \PHPMailer\PHPMailer\PHPMailer;
+use \PHPMailer\PHPMailer\SMTP;
+
+use \LogicException;
+
+class Mailer
+{
+  private static $instance;
+
+  private function __construct()
+  {
+    throw new LogicException('Mailer class should not be constructed');
+  }
+
+  public static function getInstance() : PHPMailer
+  {
+    if (!self::$instance)
+    {
+      try
+      {
+        self::$instance = new PHPMailer(true);
+        self::$instance->isSMTP();
+
+        self::$instance->Host = Common::$config->smtp->hostname;
+        self::$instance->Port = Common::$config->smtp->port;
+        self::$instance->SMTPSecure = Common::$config->smtp->tls;
+        self::$instance->SMTPKeepAlive = Common::$config->smtp->keepalive;
+
+        self::$instance->SMTPAuth = !empty(Common::$config->smtp->password);
+        self::$instance->Username = Common::$config->smtp->username;
+        self::$instance->Password = Common::$config->smtp->password;
+      }
+      catch (PHPMailerException $e)
+      {
+        self::$instance->getSMTPInstance()->reset();
+        throw $e;
+      }
+    }
+
+    self::$instance->clearAddresses();
+    self::$instance->clearAttachments();
+
+    return self::$instance;
+  }
+}
