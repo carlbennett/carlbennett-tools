852c308a9b1d71fcfa9a8df61495b42143d90900
Carl Bennett <carl@carlbennett.me>
2024-07-22 23:56:02 -0500

    Fix deprecation warnings in newer PHP

--- src/Controllers/Users.php
+++ src/Controllers/Users.php
@@ -4,29 +4,27 @@ namespace CarlBennett\Tools\Controllers;
 
 class Users extends Base
 {
-  public function __construct()
-  {
-    $this->model = new \CarlBennett\Tools\Models\Users();
-  }
-
-  public function invoke(?array $args): bool
-  {
-    if (!$this->model->active_user
-      || !$this->model->active_user->getAclObject()->getAcl(\CarlBennett\Tools\Libraries\User\Acl::ACL_USERS_MANAGE))
+    public function __construct()
     {
-      $this->model->users = false;
-      $this->model->_responseCode = 401;
-      return true;
+        $this->model = new \CarlBennett\Tools\Models\Users();
     }
 
-    $q = new \CarlBennett\Tools\Libraries\Utility\HTTPForm(\CarlBennett\Tools\Libraries\Router::query());
+    public function invoke(?array $args): bool
+    {
+        if (!$this->model->active_user || !$this->model->active_user->getAclObject()->getAcl(\CarlBennett\Tools\Libraries\User\Acl::ACL_USERS_MANAGE))
+        {
+          $this->model->_responseCode = 401;
+          return true;
+        }
 
-    $this->model->hl = $q->get('hl');
-    $this->model->id = $q->get('id');
-    $this->model->show_hidden = $q->get('sh');
-    $this->model->users = \CarlBennett\Tools\Libraries\User\User::getAll();
+        $q = new \CarlBennett\Tools\Libraries\Utility\HTTPForm(\CarlBennett\Tools\Libraries\Router::query());
 
-    $this->model->_responseCode = 200;
-    return true;
-  }
+        $this->model->hl = $q->get('hl');
+        $this->model->id = $q->get('id');
+        $this->model->show_hidden = (bool) $q->get('sh');
+        $this->model->users = \CarlBennett\Tools\Libraries\User\User::getAll();
+
+        $this->model->_responseCode = 200;
+        return true;
+    }
 }
--- src/Models/Users.php
+++ src/Models/Users.php
@@ -4,7 +4,8 @@ namespace CarlBennett\Tools\Models;
 
 class Users extends ActiveUser
 {
-  public $hl; // highlight
-  public $id;
-  public $users;
+    public ?string $hl = null; // highlight
+    public ?string $id = null;
+    public bool $show_hidden = false;
+    public ?array $users = null;
 }
