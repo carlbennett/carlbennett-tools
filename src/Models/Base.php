1d2f18cc7d992e9ac68cd195ec635e23619c20c1
Carl Bennett <carl@carlbennett.me>
2025-08-04 23:50:40 -0500

    Add additional headers

--- src/Models/Base.php
+++ src/Models/Base.php
@@ -7,7 +7,9 @@ abstract class Base implements \CarlBennett\Tools\Interfaces\Model, \JsonSeriali
     public int $_responseCode = \CarlBennett\Tools\Libraries\Core\HttpCode::HTTP_INTERNAL_SERVER_ERROR;
     public array $_responseHeaders = [
         'Cache-Control' => 'max-age=0,no-cache,no-store', // disables cache in the browser for all PHP pages by default.
-        'X-Frame-Options' => 'DENY' // DENY tells the browser to prevent archaic frame/iframe embeds of all pages including from ourselves (see also: SAMEORIGIN).
+        'X-Content-Type-Options' => 'nosniff', // Prevent browsers from incorrectly detecting non-scripts as scripts.
+        'X-Frame-Options' => 'DENY', // DENY tells the browser to prevent archaic frame/iframe embeds of all pages including from ourselves (see also: SAMEORIGIN).
+        'X-XSS-Protection' => '1;mode=block', // Block pages from loading when they detect reflected XSS attacks.
     ];
 
     public function jsonSerialize(): mixed
